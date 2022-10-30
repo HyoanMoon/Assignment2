c377b57c81534ce3f76e2c724baf491f504ad124
문효안 <hyoanmoon@munhyoan-ui-MacBookAir.local>
2022-10-30 00:22:32 -0400

    initial commit

---
+++ Server/Controllers/movie-list.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+
+/**  Temporary section **/
+
+import Movie from '../Models/movie';
+
+import { UserDisplayName } from '../Util'; 
+
+export function DisplayMovieList(req: express.Request, res: express.Response , next: express.NextFunction)
+{
+  Movie.find(function(err, moviesCollections)
+  {
+    if(err)
+    {
+        console.error(err);
+        res.end(err);
+    }
+    res.render('index', {title: 'Movie List', page: 'movie-list', movies: moviesCollections, displayName: UserDisplayName(req)});
+
+  })
+}
